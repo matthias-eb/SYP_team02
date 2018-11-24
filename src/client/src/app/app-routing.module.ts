df4bf4ae11af65828d6c37a558f7d5ddb658b238
Simon Kreuziger <simon.kreuziger@smail.th-koeln.de>
2018-11-24 17:42:32 +0100

    Navigation: 'href' mit 'routerLink' ersetzt,  Anmelden: erweitert

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MainComponent } from './main/main.component';
+import { AnmeldenComponent } from './pages/anmelden/anmelden.component';
 
 const routes: Routes = [
-  { path: '', component: MainComponent }
+  { path: '', component: MainComponent },
+  { path: 'anmelden', component: AnmeldenComponent }
 ];
 
 @NgModule({
--- src/client/src/app/pages/anmelden/anmelden.component.ts
+++ src/client/src/app/pages/anmelden/anmelden.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { AuthentificationService } from '../../services/authentification.service';
 
 @Component({
   selector: 'app-anmelden',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./anmelden.component.scss']
 })
 export class AnmeldenComponent implements OnInit {
+  private userId: number;
+  name = new FormControl('');
+  password = new FormControl('');
 
   constructor() { }
 
   ngOnInit() {
   }
+  private statusMessage = '';
+  private loginMessage = '';
+  private type = '';
+  onSubmit(){
 
+    if(this.name.value == '' || this.password.value == '' ){
+      this.type = 'danger';
+      this.loginMessage='Bitte Namen und Passwort angeben.';
+    }else{
+      /*
+      let service: AuthentificationService;
+
+      service.login(this.name.value, this.password.value).subscribe((data:any) => {
+        this.statusMessage=data.msg;
+        //this.userId=data.user.id;
+      });
+      
+      this.type = 'success';
+      this.loginMessage=this.statusMessage;
+      */
+
+      this.type = 'success';
+      this.loginMessage='TODO: Erreichen von Backend-Server-Seite';
+
+    }
+  }
+
+  
 }
