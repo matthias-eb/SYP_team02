f4fb5c3f89134230a6dd6147216956317e09d078
Matthias Eberlein <matthias_leonhard.eberlein@smail.th-koeln.de>
2018-12-04 14:11:54 +0100

    LAdestation fehler behoben

--- src/server/src/models/powerstations.js
+++ src/server/src/models/powerstations.js
@@ -68,7 +68,7 @@ let powerstations = db.sequelize.define('powerstations', {
   // don't delete database entries but set the newly added attribute deletedAt
   // to the current date (when deletion was done). paranoid will only work if
   // timestamps are enabled
-  paranoid: true,
+  paranoid: false,
 
   // if true:
   // don't use camelcase for automatically added attributes but underscore style
