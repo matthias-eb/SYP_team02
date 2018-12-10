19369d1191a2064d01e776331e7e31b19d1debc1
Matthias Eberlein <matthias_leonhard.eberlein@smail.th-koeln.de>
2018-12-10 20:02:27 +0100

    Testdatensatz korrigiert.

--- src/server/src/models/manufacturer.js
+++ src/server/src/models/manufacturer.js
@@ -25,7 +25,7 @@ manufacturer = db.sequelize.define('manufacturer', {
   // don't delete database entries but set the newly added attribute deletedAt
   // to the current date (when deletion was done). paranoid will only work if
   // timestamps are enabled
-  paranoid: true,
+  paranoid: false,
 
   // if true:
   // don't use camelcase for automatically added attributes but underscore style
