189fe462b296ff3115724f41280c567b3a27f415
Fahri Kus <fahri.kus@smail.th-koeln.de>
2019-01-11 12:39:30 +0100

    Create Statements angepasst. Testdatensätze für alle Filter erstellt. Systemspezifikation für die Filtertypen angepasst. Aussehen der erweiterten Suche weiter angepasst.

---
+++ docs/Systemdokumentation/Drop Tables.sql
@@ -0,0 +1,7 @@
+DROP TABLE Filter;
+DROP TABLE Bewertungen;
+DROP TABLE Benutzer;
+DROP TABLE Ladestationen;
+DROP TABLE Werkstaetten;
+DROP TABLE Elektroautos;
+DROP TABLE Hersteller;
--- src/client/src/app/pages/filterkomponenten/dropdown/dropdown.component.ts
+++ src/client/src/app/pages/filterkomponenten/dropdown/dropdown.component.ts
@@ -8,9 +8,23 @@ import {Component, Input, OnInit} from '@angular/core';
 export class DropdownComponent implements OnInit {
 
   @Input() dropdown: Object;
-  constructor() { }
+
+  private userFilter: Object;
+
+  constructor() {
+    this.userFilter = {};
+  }
 
   ngOnInit() {
   }
 
+
+  onChange(filter, value) {
+    if (value === '') {
+      delete this.userFilter[filter];
+    } else {
+      this.userFilter[filter] = value;
+      console.log('Selected car', value);
+    }
+  }
 }
